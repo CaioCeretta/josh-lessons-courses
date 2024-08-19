5638fe61c195103b8c59b3f8f0fdedbc072a29b7
andandtex <andandtex@gmail.com>
2024-08-19 00:17:03 -0300

    feat: explanation start, animation home page

--- lessons/src/app/animation/animated-background/page.tsx
+++ lessons/src/app/animation/animated-background/page.tsx
@@ -4,17 +4,17 @@ const AnimatedBackground = () => {
   return (
     <>
       <div className="mx-auto w-[700px]">
-        <h1 className="text-[2rem] font-bold text-center">Animated Tabs Examples</h1>
+        <h1 className="text-[2rem] font-bold text-center my-5">Animated Tabs Examples</h1>
 
-        <div className="flex flex-col">
-          <ul>
-            <li><Link href='./animated-background/AnimatedTabs'>Animated Tabs</Link></li>
-            <li><Link href='./animated-background/AnimatedTabsHover'>Animated Tabs Hover</Link></li>
-            <li><Link href='./animated-background/AnimatedCardBackground'>Animated Card Background</Link></li>
-            <li><Link href='./animated-background/SegmentedControl'>Segmented Control</Link></li>
-            
+        <div className="flex flex-col items-center my-10 gap-5">
 
-          </ul>
+          <div className="border p-5 rounded-full"><Link href='./animated-background/AnimatedTabs'>Animated Tabs</Link></div>
+          <div className="border p-5 rounded-full"><Link href='./animated-background/AnimatedTabsHover'>Animated Tabs Hover</Link></div>
+          <div className="border p-5 rounded-full"><Link href='./animated-background/AnimatedCardBackground'>Animated Card Background</Link></div>
+          <div className="border p-5 rounded-full"><Link href='./animated-background/SegmentedControl'>Segmented Control</Link></div>
+
+
+
         </div>
       </div>
     </>
